880cfa6d3558de599f2fd453af958438eccf077e
Rajesh <veeramalli.rajesh@yahoo.com>
2020-03-25 14:30:54 +0100

    Updates

---
+++ precache-manifest.7255a6aa5744494ebccdd5c5b7eb0528.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0acbb2febf39bdad2501bf28e7a6b8ad",
+    "url": "/portfolio-create-react-app/index.html"
+  },
+  {
+    "revision": "1e0dc62ce6ec0da77241",
+    "url": "/portfolio-create-react-app/static/css/main.2bad1df7.chunk.css"
+  },
+  {
+    "revision": "f139f8e7713735b61224",
+    "url": "/portfolio-create-react-app/static/js/2.499a16da.chunk.js"
+  },
+  {
+    "revision": "1e0dc62ce6ec0da77241",
+    "url": "/portfolio-create-react-app/static/js/main.0a0124c5.chunk.js"
+  },
+  {
+    "revision": "54f6b933a5bcba86415a",
+    "url": "/portfolio-create-react-app/static/js/runtime-main.2c4c3d8c.js"
+  }
+]);
