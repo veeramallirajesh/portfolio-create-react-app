e07b515ca661243d26db8bcc567d35068b2c1916
Rajesh <veeramalli.rajesh@yahoo.com>
2020-03-25 13:56:17 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import Pages from './pages';
+
+function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    //use name of the gitconnected profile.
+    fetch('https://gitconnected.com/v1/portfolio/veeramallirajesh')
+      .then(res => res.json())
+      .then(user => {
+        setUser(user);
+      });
+  }, []);
+
+  if (!user) {
+    return <div />;
+  }
+
+  return <Pages user={user} />;
+}
+
+export default App;
